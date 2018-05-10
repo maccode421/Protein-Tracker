708d6057d1830a95a9fe2d443bb1cf445b0b7e9a
maccode421 <maccavalli@yahoo.com>
2018-05-10 17:55:53 -0400

    Populating user data and history template

--- client/main.js
+++ client/main.js
@@ -3,3 +3,26 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+Template.userDetails.helpers({
+  user: function() {
+    var user = {
+      total: 123,
+      goal: 251
+    }
+    return user
+  }
+})
+
+Template.history.helpers({
+  historyItem: function() {
+    var historyItems = 
+    [
+      { date: '5/11/2018 5:46 PM', value: 21 },
+      { date: '5/11/2018 5:46 PM', value: 22 },
+      { date: '5/11/2018 5:46 PM', value: 23 },
+      { date: '5/11/2018 5:46 PM', value: 24 },
+      { date: '5/11/2018 5:46 PM', value: 25 }
+    ]
+    return historyItems
+  }
+})
