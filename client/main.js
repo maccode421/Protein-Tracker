7c55273eb0b560a6aecad286e5940bf53301a565
maccode421 <maccavalli@yahoo.com>
2018-05-10 18:21:00 -0400

    Adding a User and a History Collection

--- client/main.js
+++ client/main.js
@@ -3,26 +3,17 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+Users = new Meteor.Collection('users')
+History = new Meteor.Collection('history')
+
 Template.userDetails.helpers({
   user: function() {
-    var user = {
-      total: 123,
-      goal: 251
-    }
-    return user
+    return Users.findOne()
   }
 })
 
 Template.history.helpers({
   historyItem: function() {
-    var historyItems = 
-    [
-      { date: '5/11/2018 5:46 PM', value: 21 },
-      { date: '5/11/2018 5:46 PM', value: 22 },
-      { date: '5/11/2018 5:46 PM', value: 23 },
-      { date: '5/11/2018 5:46 PM', value: 24 },
-      { date: '5/11/2018 5:46 PM', value: 25 }
-    ]
-    return historyItems
+    return History.find()
   }
 })
--- server/main.js
+++ server/main.js
@@ -1,5 +1,30 @@
 import { Meteor } from 'meteor/meteor';
 
+Users = new Meteor.Collection('users')
+History = new Meteor.Collection('history')
+
 Meteor.startup(() => {
   // code to run on server at startup
+
+  if (Users.find().count() === 0) {
+    Users.insert({
+      total: 120,
+      goal: 200
+    })
+  }
+
+  if (History.find().count() === 0) {
+    History.insert({
+      value: 20,
+      date: new Date().toTimeString()
+    })
+    History.insert({
+      value: 40,
+      date: new Date().toTimeString()
+    })
+    History.insert({
+      value: 30,
+      date: new Date().toTimeString()
+    })
+  }
 });
